9cc79714220ea50e1bd1880a2b2d1f4256abb70f
justjcurtis <justjcurtis@gmail.com>
2024-08-13 15:03:31 +0100

    test: stringUtils/capitalise

---
+++ src/utils/stringUtils.test.js
@@ -0,0 +1,16 @@
+import { capitalise } from './stringUtils';
+
+const capitaliseTestCases = [
+    ['hello', 'Hello'],
+    ['hello world', 'Hello World'],
+    ['hello woRLd', 'Hello WoRLd'],
+    ['hello world!', 'Hello World!'],
+    ['0000', '0000'],
+    ['hello world 123', 'Hello World 123'],
+    ['hello World 123!', 'Hello World 123!'],
+    ['!!!!!', '!!!!!'],
+];
+
+it.each(capitaliseTestCases)('capitalize(%s) should return %s', (input, expected) => {
+    expect(capitalise(input)).toBe(expected);
+})
