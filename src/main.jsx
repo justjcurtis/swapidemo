78dd3945b9388e7750aaacbb2249f0f2a88b6780
justjcurtis <justjcurtis@gmail.com>
2024-08-13 14:33:18 +0100

    chore: wrap app with context providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { StarshipProvider } from './contexts/starshipContext.jsx'
+import { NotificationProvider } from './contexts/notification.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <NotificationProvider>
+      <StarshipProvider>
+        <App />
+      </StarshipProvider>
+    </NotificationProvider>
+  </StrictMode >,
 )
