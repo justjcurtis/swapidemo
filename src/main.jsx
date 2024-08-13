4dca831e1a14df6e36624d877798d89d2f902c0b
justjcurtis <justjcurtis@gmail.com>
2024-08-13 16:42:18 +0100

    chore: update context imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { StarshipProvider } from './contexts/starshipContext.jsx'
-import { NotificationProvider } from './contexts/notification.jsx'
+import { StarshipProvider } from './contexts/starshipContext'
+import { NotificationProvider } from './contexts/notificationContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
