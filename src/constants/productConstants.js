2b4011ea32d07c9e865308ce462386d265843846
justjcurtis <justjcurtis@gmail.com>
2024-08-13 14:35:11 +0100

    feat: create product card constants

---
+++ src/constants/productConstants.js
@@ -0,0 +1,16 @@
+export const DATA_ROW_HEADERS = {
+    starship_class: "Class",
+    manufacturer: "Manufacturer",
+    model: "Model",
+    cargo_capacity: "Cargo Capacity",
+    passengers: "Passengers",
+    crew: "Crew",
+    consumables: "Consumables",
+    MGLT: "Megalights",
+    hyperdrive_rating: "Hyperdrive Rating",
+    max_atmosphering_speed: "Max Atmosphering Speed",
+    length: "Length",
+    cost_in_credits: "Cost",
+}
+
+export const CURRENCY_SYMBOL = "₹";
