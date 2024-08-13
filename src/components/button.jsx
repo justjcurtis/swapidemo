39d008d725743956800da72035c8bc92c2612e11
justjcurtis <justjcurtis@gmail.com>
2024-08-13 14:32:54 +0100

    chore: delete old button

--- src/components/button.jsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@carbon/react';
-import { Add } from '@carbon/react/icons';
-
-function AddButton({ onClick }) {
-  return <Button onClick={onClick}><Add /></Button>;
-}
-
-export default AddButton;
