1059b005888e5d839c73455891166ee1b9de5f83
justjcurtis <justjcurtis@gmail.com>
2024-08-14 00:01:49 +0100

    feat: add units to all info possible and create separate methods to do so

--- src/components/dataRow.jsx
+++ src/components/dataRow.jsx
@@ -12,10 +12,44 @@ const styles = {
   },
 }
 
-const DataRow = ({ title, value, light }) => {
+const handleCost = (cost) => {
+  cost = parseInt(cost)
+  if (isNaN(cost)) return "Unknown"
+  return `${CURRENCY_SYMBOL}${cost.toLocaleString()}`
+}
+
+const handleNumber = (value) => {
+  if (value.toLowerCase() === "n/a") return value.toUpperCase()
+  const parsed = parseInt(value)
+  if (isNaN(parsed)) return value
+  return parsed.toLocaleString()
+
+}
+
+const handleValue = (title, value) => {
   value = capitalise(value)
-  if (title == "Cost") value = `${CURRENCY_SYMBOL}${value}`;
+  if (title == "Cost") value = handleCost(value);
+  value = handleNumber(value);
   if (title == "Megalights") value = `${value} MGLT`;
+  switch (title) {
+    case "Megalicghts":
+      return `${value} MGLT`
+    case "Cargo Capacity":
+      return `${value} kg`
+    case "Consumables":
+      return `${value} years`
+    case "Max Atmosphering Speed":
+      if (value !== "N/A") return `${value} km/h`
+      break
+    case "Length":
+      return `${value} m`
+  }
+
+  return value
+}
+
+const DataRow = ({ title, value, light }) => {
+  value = handleValue(title, value)
   return (
     <div style={{ ...styles.rowStyle, backgroundColor: light ? "#666" : "#333" }}>
       <span style={{ textAlign: "left", fontWeight: 'bold' }}>{title}: </span>
